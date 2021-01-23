6cd32696129bcc7a8b44e806f36d93dadf16e2b7
Mohamed <m.shmayssany@gmail.com>
2021-01-23 23:43:21 +0100

    mvc

---
+++ app/config/config.php
@@ -0,0 +1,19 @@
+<?php
+// DB define
+define('DB_HOST', 'localhost');
+define('DB_USER', 'mohamed');
+define('DB_PASS', 'mkahms');
+define('DB_NAME', 'shareposts');
+
+// App Root
+define('APPROOT', dirname(dirname(__FILE__)));
+
+// URL Root
+
+define('URLROOT', 'http://mvc.lrn');
+
+// Site name
+define('SITENAME', 'SharePosts');
+
+// App version
+define('APPVERSION', '1.0.0');
